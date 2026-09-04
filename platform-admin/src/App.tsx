7b3674b9fe4b12f722755e2e7ae38cf6fd08cc11
Md Arman Ansari <armanmuz951@gmail.com>
2026-09-04 23:05:08 +0530

    theme setup

--- platform-admin/src/App.tsx
+++ platform-admin/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
-    <>
+    <div className='font-bold text-red-500'>
      Hello
-    </>
+     <Button>Click Me</Button>
+    </div>
   )
 }
 
--- platform-admin/vite.config.ts
+++ platform-admin/vite.config.ts
@@ -1,11 +1,28 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3002,
+    strictPort: true,
+  },
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
+    tailwindcss(),
   ],
 })
